c323ac55af5f4be62a166f77900b66dd88df321f
Vasu Bhalodi <vasubhalodi111@gmail.com>
2024-02-07 23:11:03 +0530

    shopping-cart-localstorage: add filter options - pending, completed, past task, priority

--- javascript/todo-localstorage/main.js
+++ javascript/todo-localstorage/main.js
@@ -1,42 +1,49 @@
-const LOCALSTORAGE = {
-  todos: "todos",
-};
-const SESSIONSTORAGE = {
-  id: "id",
-};
-var isUpdateMode = false;
+const LOCALSTORAGE_TODOS = "todos";
+const SESSIONSTORAGE_ID = "id";
 
-// Add and Update
+function fetchLocalStorageTodos() {
+  return JSON.parse(localStorage.getItem(LOCALSTORAGE_TODOS));
+}
+function saveLocalStorageTodos(todo) {
+  localStorage.setItem(LOCALSTORAGE_TODOS, JSON.stringify(todo));
+}
+
+function fetchSessionStorageId() {
+  return JSON.parse(sessionStorage.getItem(SESSIONSTORAGE_ID));
+}
+function saveSessionStorageId(id) {
+  sessionStorage.setItem(SESSIONSTORAGE_ID, JSON.stringify(id));
+}
+
+var isUpdateMode = false;
+var selectFilter = null;
+
 const title = document.getElementById("title");
 const dueDate = document.getElementById("date");
 const priority = document.getElementById("priority");
 const createBtn = document.getElementById("create-btn");
+
+const todoList = document.getElementById("todoList");
+const filterTodo = document.getElementById("filterTodo");
+
+// Add and Update
 createBtn.addEventListener("click", () => {
-  if (!isUpdateMode) {
-    const status = AddTodo();
-    if (status) {
-      console.log("todo created!");
-    } else {
-      console.log("something went wrong!");
-      return;
-    }
-  } else {
-    const status = UpdateTodo();
-    if (status) {
-      console.log("todo updated!");
-      isUpdateMode = false;
-    } else {
-      console.log("something went wrong!");
-      return;
-    }
+  if (!title.value || !dueDate.value) {
+    alert("require all fields");
+    return;
   }
 
+  if (!isUpdateMode) {
+    AddTodo();
+  } else {
+    UpdateTodo();
+  }
   clearFields();
-  loadTodo();
+  filterTodoList(selectFilter);
 });
 
 function AddTodo() {
-  const fetchTodo = JSON.parse(localStorage.getItem(LOCALSTORAGE.todos)) || [];
+  const fetchTodo = fetchLocalStorageTodos();
   const newTodo = {
     id: new Date().getTime(),
     title: title.value,
@@ -45,20 +52,20 @@ function AddTodo() {
     complete: false,
   };
   const mergeTodo = [...fetchTodo, newTodo];
-  localStorage.setItem(LOCALSTORAGE.todos, JSON.stringify(mergeTodo));
+  saveLocalStorageTodos(mergeTodo);
   return true;
 }
 
 function UpdateTodo() {
-  const fetchTodo = JSON.parse(localStorage.getItem(LOCALSTORAGE.todos)) || [];
-  const fetchId = JSON.parse(sessionStorage.getItem(SESSIONSTORAGE.id));
+  const fetchTodo = fetchLocalStorageTodos();
+  const fetchId = fetchSessionStorageId();
   const fetchIndex = fetchTodo.findIndex((item) => item.id === fetchId);
 
   fetchTodo[fetchIndex].title = title.value;
   fetchTodo[fetchIndex].dueDate = dueDate.value;
   fetchTodo[fetchIndex].priority = priority.checked;
 
-  localStorage.setItem(LOCALSTORAGE.todos, JSON.stringify(fetchTodo));
+  saveLocalStorageTodos(fetchTodo);
   return true;
 }
 
@@ -72,105 +79,117 @@ function clearFields() {
 }
 
 // Load Todo List
-const todoList = document.getElementById("todoList");
-const filterTodo = document.getElementById("filterTodo");
-
 filterTodo.addEventListener("change", (e) => {
-  loadTodo(e.target.value);
+  filterTodoList(e.target.value);
 });
 
-loadTodo(null);
-function loadTodo(value) {
+filterTodoList(null);
+function filterTodoList(value) {
   todoList.innerHTML = "";
-  const fetchTodos = JSON.parse(localStorage.getItem(LOCALSTORAGE.todos)) || [];
+  selectFilter = value;
+  const fetchTodos = fetchLocalStorageTodos();
 
-  let filterRecord = null;
-  if (value === null || value === undefined || value === "") {
-    filterRecord = fetchTodos;
+  let filterRecord = {};
+
+  switch (value) {
+    case "Pending":
+      filterRecord = fetchTodos.filter((todo) => !todo.complete);
+      break;
+    case "Completed":
+      filterRecord = fetchTodos.filter((todo) => todo.complete);
+      break;
+    case "Past Task":
+      const currentDate = new Date();
+      filterRecord = fetchTodos.filter((todo) => {
+        const dueDate = new Date(todo.dueDate);
+        return currentDate > dueDate;
+      });
+      break;
+    case "Priority":
+      filterRecord = fetchTodos.filter((todo) => todo.priority);
+      break;
+    default:
+      filterRecord = fetchTodos;
   }
 
-  if (value === "Pending") {
-    filterRecord = fetchTodos.filter((todo) => todo.complete === false);
-  }
-  if (value === "Completed") {
-    filterRecord = fetchTodos.filter((todo) => todo.complete === true);
-  }
-  if (value === "Due") {
-    const currDate = new Date();
-    filterRecord = fetchTodos.filter((todo) => {
-      const date = new Date(todo.dueDate);
-      return currDate > date;
-    });
-  }
-  if (value === "Priority") {
-    filterRecord = fetchTodos.filter((todo) => todo.priority === true);
-  }
+  const sortTodoCompleted = sortTodoList(filterRecord);
 
-  const sortTodoCompleted = filterRecord.sort((a, b) => {
-    return a.complete - b.complete;
-  });
-
-  if (!sortTodoCompleted.length) {
+  if (sortTodoCompleted.length === 0) {
     const createDiv = document.createElement("div");
     createDiv.innerHTML = `
         <div class='center'>todo not found!</div>
     `;
     todoList.appendChild(createDiv);
     return;
+  } else {
+    sortTodoCompleted.forEach((todo) => {
+      const todoItem = createTodoListElement(todo);
+      todoList.appendChild(todoItem);
+    });
   }
+}
 
-  sortTodoCompleted.forEach((element) => {
-    const createDiv = document.createElement("div");
-    createDiv.className = `task ${element.complete ? "complete-color" : ""}`;
-
-    createDiv.innerHTML += `
-        <div class="task-details">
-            <div class="task-title">${element.title}</div>
-            <div class="due-date">${element.dueDate}</div>
-        </div>
-        <div class="task-actions">
-            <button class="complete-btn" onclick='completeTodo(${element.id})'><i class="fas fa-check-circle"></i></button>
-            <button class="delete-btn" onclick='deleteTodo(${element.id})'><i class="fas fa-trash"></i></button>
-            <button class="edit-btn" onclick='openUpdateModal(${element.id})'><i class="fas fa-edit"></i></button>
-        </div>
-
-    `;
-
-    todoList.appendChild(createDiv);
+function sortTodoList(todo) {
+  return todo.sort((a, b) => {
+    return a.complete - b.complete;
   });
+}
+
+function createTodoListElement(todo) {
+  const createDiv = document.createElement("div");
+  createDiv.className = `task ${todo.complete ? "complete-color" : ""}`;
+  createDiv.innerHTML += `
+      <div class="task-details">
+          <div class="task-title">${todo.title}</div>
+          <div class="due-date">${todo.dueDate}</div>
+      </div>
+      <div class="task-actions">
+          <button class="complete-btn" onclick='completeTodo(${todo.id})'> ${
+    todo.complete
+      ? '<i class="fas fa-check-circle"></i>'
+      : '<i class="far fa-check-circle"></i>'
+  } </button>
+          <button class="delete-btn" onclick='deleteTodo(${
+            todo.id
+          })'><i class="fas fa-trash"></i></button>
+          <button class="edit-btn" onclick='openUpdateModal(${
+            todo.id
+          })'><i class="fas fa-edit"></i></button>
+      </div>
+    `;
+  return createDiv;
 }
 
 // Complete Todo
 function completeTodo(id) {
-  const fetchTodos = JSON.parse(localStorage.getItem(LOCALSTORAGE.todos)) || [];
+  const fetchTodos = fetchLocalStorageTodos();
   const todoIndex = fetchTodos.findIndex((todo) => todo.id === id);
 
   if (todoIndex !== -1) {
     fetchTodos[todoIndex].complete = !fetchTodos[todoIndex].complete;
-    localStorage.setItem(LOCALSTORAGE.todos, JSON.stringify(fetchTodos));
+    saveLocalStorageTodos(fetchTodos);
   }
-  loadTodo(null);
+  filterTodoList(selectFilter);
 }
 
 // Delete Todo
 function deleteTodo(id) {
   const response = confirm("Are you sure you want to delete?");
   if (response) {
-    const fetchTodos =
-      JSON.parse(localStorage.getItem(LOCALSTORAGE.todos)) || [];
+    const fetchTodos = fetchLocalStorageTodos();
     if (!fetchTodos.length) return;
 
     const filterRecord = fetchTodos.filter((record) => {
       return record.id !== id;
     });
-    localStorage.setItem(LOCALSTORAGE.todos, JSON.stringify(filterRecord));
-    loadTodo(null);
+    saveLocalStorageTodos(filterRecord);
+    filterTodoList(selectFilter);
   }
 }
 
 // Open Update Modal
 function openUpdateModal(id) {
-  const fetchTodos = JSON.parse(localStorage.getItem(LOCALSTORAGE.todos)) || [];
+  const fetchTodos = fetchLocalStorageTodos();
   const findTodo = fetchTodos.find((item) => item.id === id);
   modal.style.display = "block";
 
@@ -180,7 +199,8 @@ function openUpdateModal(id) {
   priority.checked = findTodo.priority;
   document.getElementById("modal-title").innerHTML = "Update Todo";
   document.getElementById("create-btn").innerHTML = "Update";
-  sessionStorage.setItem(SESSIONSTORAGE.id, JSON.stringify(findTodo.id));
+
+  saveSessionStorageId(findTodo.id);
 }
 
 // Handle Modal Event
