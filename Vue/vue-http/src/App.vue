259e4a02e7167b172b7783b568afc9aac4592a81
vasubhalodi07 <vasubhalodi111@gmail.com>
2024-05-20 13:46:41 +0530

    vue-http: get request with axios in click event

---
+++ Vue/vue-http/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <FetchPost />
+  </div>
+</template>
+
+<script>
+import FetchPost from "./components/FetchPost.vue";
+
+export default {
+  name: "App",
+  components: {
+    FetchPost,
+  },
+};
+</script>
+
+<style></style>
---
+++ Vue/vue-http/src/components/FetchPost.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="fetchPosts">Load Posts</button>
+
+    <div class="load-posts" v-for="post in posts" :key="post.id">
+      {{ post.id }} {{ post.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "FetchPost",
+  data() {
+    return {
+      posts: [],
+      errorMessage: "",
+    };
+  },
+  methods: {
+    fetchPosts() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => {
+          console.log(res.data);
+          this.posts = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.errorMessage = "error in fetching posts";
+        });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
