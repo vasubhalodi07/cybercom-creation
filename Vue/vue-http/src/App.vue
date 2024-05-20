d90a7c4506e231bdd821089ed13b32a8044597c4
vasubhalodi07 <vasubhalodi111@gmail.com>
2024-05-20 14:30:50 +0530

    vue-http: implement template refs

--- Vue/vue-http/src/App.vue
+++ Vue/vue-http/src/App.vue
@@ -2,18 +2,21 @@
   <div>
     <AddPost />
     <FetchPost />
+    <TemplateRef />
   </div>
 </template>
 
 <script>
 import FetchPost from "./components/FetchPost.vue";
 import AddPost from "./components/AddPost.vue";
+import TemplateRef from "./components/TemplateRef.vue";
 
 export default {
   name: "App",
   components: {
     FetchPost,
     AddPost,
+    TemplateRef,
   },
 };
 </script>
---
+++ Vue/vue-http/src/components/TemplateRef.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" ref="inputRef" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TemplateRef",
+  mounted() {
+    this.$refs.inputRef.focus();
+  },
+};
+</script>
+
+<style scoped></style>
