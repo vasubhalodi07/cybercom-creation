8796256d0cf7552e7a037438f8734a5033206d52
vasubhalodi07 <vasubhalodi111@gmail.com>
2024-05-20 13:59:19 +0530

    vue-http: post request with axios in click event

--- Vue/vue-http/src/App.vue
+++ Vue/vue-http/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <AddPost />
     <FetchPost />
   </div>
 </template>
 
 <script>
 import FetchPost from "./components/FetchPost.vue";
+import AddPost from "./components/AddPost.vue";
 
 export default {
   name: "App",
   components: {
     FetchPost,
+    AddPost,
   },
 };
 </script>
---
+++ Vue/vue-http/src/components/AddPost.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form @submit.prevent="addPost()">
+      <input type="text" v-model="formData.title" />
+      <input type="text" v-model="formData.body" />
+      <input type="submit" value="Add Post" />
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "AddPost",
+  data() {
+    return {
+      formData: {
+        title: "",
+        body: "",
+        userId: 1,
+      },
+    };
+  },
+  methods: {
+    addPost() {
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", this.formData)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
