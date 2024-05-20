d810a4405d4b8628269d4b4f8975c597155a0ff3
vasubhalodi07 <vasubhalodi111@gmail.com>
2024-05-20 18:39:19 +0530

    vue-router: implement routing

---
+++ Vue/vue-routing/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "vue/multi-word-component-names": "off", // Disable the rule globally
+    // other rules
+  },
+};
---
+++ Vue/vue-routing/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/product">Product</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line vue/multi-word-component-names
+
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+nav {
+  display: flex;
+  gap: 20px;
+}
+</style>
